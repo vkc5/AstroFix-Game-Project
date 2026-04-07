d13d039411e25a00a71239ce8c1a7f28244427e9
Mohammed Alhalal <126599773+vkc5@users.noreply.github.com>
2026-04-07 15:11:04 +0300

    Project structure setup

---
+++ Assets/AssetStore/SciFiCorridorSet/Scripts/LightCone.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightCone : MonoBehaviour {
+
+	public Transform mCameraTr;
+	
+	void Start()
+	{
+		if (!mCameraTr)
+			mCameraTr = Camera.main.transform;
+	}
+	
+	void Update()
+	{
+		transform.LookAt(transform.position + mCameraTr.rotation * Vector3.forward, mCameraTr.rotation * Vector3.up);
+	}
+}
---
+++ Assets/AssetStore/SciFiCorridorSet/Scripts/LightFlashing.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlashing : MonoBehaviour
+ {
+	
+	private Material _material;
+	
+	public Renderer objToFlash;
+	
+	[Header("Emisson Color")]
+	public Color baseColor = Color.white;
+	
+	[Header("Emisson Strength")]
+	public float minEmisson = 0.1f;
+	public float maxEmisson = 1.1f;
+	
+	[Header("Time")]
+	public float freqrency = 0.5f;
+	
+	void Start () 
+	{
+		Random.seed = Random.Range(0, int.MaxValue);
+		
+		// If there is no paramter then search it in children
+		if(objToFlash == null) 
+			objToFlash = gameObject.GetComponentInChildren<Renderer>();
+		
+		_material = objToFlash.material;
+	}
+	
+	void Update () 
+	{
+		//float emission = minEmisson + Mathf.PingPong (Time.time * freqrency, maxEmisson - minEmisson);
+		float emission = Mathf.PingPong (Time.time * freqrency, Random.Range(minEmisson, maxEmisson));
+		Color finalColor = baseColor * Mathf.LinearToGammaSpace (emission);
+		
+		_material.SetColor ("_EmissionColor", finalColor);
+	}
+}
