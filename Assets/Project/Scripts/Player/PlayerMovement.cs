8d0e6c69ade3a48506d265a917305e9ce1a70986
Mohammed Alhalal <126599773+vkc5@users.noreply.github.com>
2026-04-15 16:38:15 +0300

    Shift + W to run

--- Assets/Project/Scripts/Player/PlayerMovement.cs
+++ Assets/Project/Scripts/Player/PlayerMovement.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
+    public float walkSpeed = 3f;
+    public float runSpeed = 6f;
     public float rotationSpeed = 150f;
 
     private Animator animator;
@@ -17,12 +18,27 @@ public class PlayerMovement : MonoBehaviour
         float move = Input.GetAxisRaw("Vertical");
         float turn = Input.GetAxisRaw("Horizontal");
 
-        transform.Translate(Vector3.forward * move * speed * Time.deltaTime);
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        float currentSpeed = isRunning ? runSpeed : walkSpeed;
+
+        transform.Translate(Vector3.forward * move * currentSpeed * Time.deltaTime);
         transform.Rotate(Vector3.up * turn * rotationSpeed * Time.deltaTime);
 
         if (animator != null)
         {
-            animator.SetFloat("Speed", move);
+            float animValue = move;
+
+            if (isRunning && move > 0)
+                animValue = 2f;
+            else if (move > 0)
+                animValue = 1f;
+            else if (move < 0)
+                animValue = -1f;
+            else
+                animValue = 0f;
+
+            animator.SetFloat("Speed", animValue);
         }
     }
 }
